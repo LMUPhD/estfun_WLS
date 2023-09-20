3be170715e555ef06c649b5e9efc5214fbd3bebd
fclasse <102723471+fclasse@users.noreply.github.com>
2023-09-20 16:22:16 +0200

    20.09.2023: Trying out how to compute joint probabilities

--- support.R
+++ support.R
@@ -40,38 +40,7 @@ get_mus <- function(var){  #, th, lv, selcols, catvals
   }))
   
   
-  return(mu)
-}
-
-
-
-
-
-
-get_joint_exp <- function(c){  #catvals, th, lv
-  
-  #-> Ebene: Item zu Item
-  cat_combs = expand.grid(1:lv[c[1]],1:lv[c[2]])
-  
-  vals_var1 = unlist(catvals[c[1]])
-  vals_var2 = unlist(catvals[c[2]])
-  
-  th_var1 = c(-Inf,th[c[1]],Inf)*-1
-  th_var2 = c(-Inf,th[c[2]],Inf)*-1
-  
-  #--> Ebene Kategorie-zu-Kategorie
-  
-  mu_joint = sum( apply(cat_combs, 1L, function(x){
-    s = unlist(x+1)
-    x=unlist(x)
-    p_katkat = sum(c(pbivnorm::pbivnorm(x = th_var1[s[1]], y =th_var2[s[2]], rho = -c[3], recycle = TRUE), 
-                     pbivnorm::pbivnorm(x = th_var1[s[1]-1], y =th_var2[s[2]], rho = -c[3], recycle = TRUE)*-1,
-                     pbivnorm::pbivnorm(x = th_var1[s[1]], y =th_var2[s[2]-1], rho = -c[3], recycle = TRUE)*-1,
-                     pbivnorm::pbivnorm(x = th_var1[s[1]-1], y =th_var2[s[2]-1], rho = -c[3], recycle = TRUE)),na.rm=T)
-    vals_var1[x[1]]*vals_var2[x[2]]*p_katkat
-  }) )
-  
-  return(mu_joint)
+  return(list(mu,catprobs.item))
 }
 
 
@@ -90,6 +59,16 @@ doDummySingleVar <- function(X,lv,num){
   }
   return(Xd)
 }
+
+
+
+
+
+
+
+
+
+
 
 
 #getProbItem <- function(X,lv,thx,num){
