f83c03baa017f23a0e9c1b67943d02a60140f91e
fclasse <102723471+fclasse@users.noreply.github.com>
2023-10-17 15:57:24 +0200

    17.10.2023: tested multidimensional IRT strucchange

--- analyze_simu_multi.R
+++ analyze_simu_multi.R
@@ -1,7 +1,33 @@
 
 setwd("C:/Users/classe/Desktop/Diss/Paper3/estfun_WLS")
-source("estfun_WLS.R")
+
+estfun.MHRM <- function(modelobj){
+  model.mhrm.values <- mirt::mirt(data=simu[,1:modelobj@Data[["nitems"]]],model=model_mirt, itemtype='graded', pars = "values", method='MHRM') 
+  
+  ## We extract the estimated parameters:
+  params <- coef(modelobj, simplify=TRUE)
+  # 1. The rowwise entries of $items
+  v1 <- as.vector(t(params$items))
+  # 2. The entries of $means
+  v2 <- as.vector(params$means)
+  # 3. The lower diagonal of $cov
+  v3 <- params$cov[lower.tri(params$cov, diag = T)]
+  
+  # We concatenate these vectors and insert them into model.recoded.values:
+  model.mhrm.values$value <- c(v1, v2, v3)
+  
+  # We now evaluate the model with EM at the estimates.
+  model.mhrm.EM <- mirt::mirt(data=simu[,1:modelobj@Data[["nitems"]]],model=model_mirt, itemtype='graded', method='EM', pars = model.mhrm.values, TOL = NaN)
+  
+  # Again, we get the score contributions:
+  return(mirt::estfun.AllModelClass(model.mhrm.EM))
+  
+}
+
+
+source("estfun_WLS2.R")
 source("multi_simu.R")
+
 
 ################################################################################
 ######################### Ordinal Data ######################################### 
@@ -26,9 +52,12 @@ simu=fits_random[["data"]][["data1"]]
 
 
 #lavaan
-fit_ord <- lavaan::cfa(model_lav, data = simu, ordered = TRUE, estimator = "WLS",std.lv=F ) #estimate variances and covariances of latent variables
-colSums(estfun.WLS(fit_ord)) 
+start <- Sys.time()
+fit_ord <- lavaan::cfa(model_lav, data = simu, ordered = TRUE, estimator = "WLS",std.lv=T ) #estimate variances and covariances of latent variables
+t_lav = as.numeric(Sys.time() - start)
 
+fitMeasures(fit_ord)[['rmsea']]
+fit_ord@Model@nx.free
 
 
 #mirt
@@ -36,19 +65,14 @@ model_mirt <- mirt::mirt.model('
 Eta1 = 1-3
 Eta2 = 4-6
 Eta3 = 7-9
-Beta2 = 2,5,8
-Beta3 = 3,6,9
-COV=Eta1*Eta2*Eta3*Beta2*Beta3
-FIXED = (2-9, a1)
-FIXED = (1-3,5-9, a2)
-FIXED = (1-6,8,9, a3)
-FIXED = (1,3-9, a4)
-FIXED = (1,2,4-9, a5)') #????
+COV=Eta1*Eta2*Eta3') 
 fit_mirt <- mirt::mirt(data=simu, model=model_mirt, itemtype="graded",method='MHRM' ) #pars="values" #'EM',TOL = NaN 
-mirt::M2(fit_mirt)
+t_mirt = fit_mirt@time[["TOTAL:"]]
+
+mirt::extract.mirt(fit_mirt, what = "RMSEA")
+fit_mirt@Model[["nest"]]
 
 
-####select a simpler model!!
 
 ################################################################################
 ########################## try strucchange #####################################
@@ -57,8 +81,41 @@ simu$rand1 <- replicate(nrow(simu),sample(c(1,2,3,4,5),1,prob=c(0.1,0.2,0.4,0.2,
 simu$rand2 <- round(runif(nrow(simu),min=0,max=200),2)                               #numeric (univ)
 
 
-flucs1 <- strucchange::gefp(fit_ord, fit=NULL, scores = estfun.WLS, order.by = simu[,"rand1"]) #ordinal
-flucs2 <- strucchange::gefp(fit_ord, fit=NULL, scores = estfun.WLS, order.by = simu[,"rand2"]) #numeric
+flucs1 <- strucchange::gefp(fit_mirt, fit=NULL, scores = estfun.MHRM, order.by = simu[,"rand1"]) #ordinal
+flucs2 <- strucchange::gefp(fit_mirt, fit=NULL, scores = estfun.MHRM, order.by = simu[,"rand2"]) #numeric
 
 strucchange::sctest(flucs2, functional = strucchange::maxBB)$p.value #dm    #--> passt... andere stats funktionieren mit >25 parametern nicht
 strucchange::sctest(flucs1, functional = strucchange::ordwmax(flucs1))$p.value[1]  #wdmo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+############################## try semtree #####################################
+################################################################################
+fit_num <- lavaan::cfa(model = model_lav, data=simu, std.lv=T, meanstructure=T,estimator = "ML",do.fit=FALSE) #, do.fit=FALSE
+semtree_simu <- semtree::semtree(model=fit_ord, data=simu, predictors=c("rand1","rand2"), control=semtree::semtree.control(method="score",bonferroni=TRUE)) 
+
+#plot.semtree aus github kopieren...
+nodeFunSemtree<-function(x, labs, digits, varlen)
+{
+  paste("n=",x$frame$n)
+}
+plot.semtree(semtree_simu)
+
+
+####Fazit: SEMTree geht noch nicht!
+
